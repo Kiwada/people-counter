05642949d575be4c3f7fbf908918529aed85a3a3
Kiwada <mamakaiofrep@gmail.com>
2025-08-28 15:40:47 -0300

    feat :layout para o formulario

--- tpircsava 1/main.js
+++ tpircsava 1/main.js
@@ -20,11 +20,33 @@ class ValidaFormulario {
         camposSaoValidos(){
             let valid = true;
 
+//Pega todos os elementos dentro de this.formulario e  O resultado é uma lista de nós (NodeList).
+//Vai percorrer cada elemento dessa lista, e em cada volta da repetição o elemento atual é guardado na variável campo.
+
 
             for(let campo of this.formulario.querySelectorAll('.validar')){
-                console.log('Show')
+                const label = campo.previousElementSibling.innerText; //Acessa o elemento irmão imediatamente anterior ao campo no HTML (geralmente um <label>).
+            
+//Esse código percorre todos os campos que precisam ser validados (.validar) e pega o texto do label imediatamente antes de cada campo, guardando na constante label.
+
+
+                if(! campo.value){
+                    this.criaErro(campo, 'Esse Campo não pode estar em branco.');
+                }
+            valid = false;
             }
+        }
+
+        criaErro(campo , msg) {
+            const div = document.createElement('div');
+            div.innerHTML = msg;
+            div.classList.add('error - text ');
+            campo.insertAdjacenteElement('afterend');
         }
 }
 
 const valida = new ValidaFormulario();
+
+
+
+
