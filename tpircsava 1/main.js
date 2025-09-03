1783f9cdf93926d66cc6c6c447cc02b1d2f6db83
Kiwada <mamakaiofrep@gmail.com>
2025-09-03 00:10:21 -0300

    feat : ajustando

--- tpircsava 1/main.js
+++ tpircsava 1/main.js
@@ -1,52 +1,108 @@
 class ValidaFormulario {
-    constructor(){
-        this.formulario = document.querySelector('.formulario')
-        this.eventos();
+  constructor() {
+    this.formulario = document.querySelector('.formulario');
+    this.eventos();
+  }
+
+  eventos() {
+    this.formulario.addEventListener('submit', e => {
+      this.handleSubmit(e);
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const camposValidos = this.camposSaoValidos();
+    const senhasValidas = this.senhasSaoValidas();
+
+    if(camposValidos && senhasValidas) {
+      alert('Formulário enviado.');
+      this.formulario.submit();
+    }
+  }
+
+  senhasSaoValidas() {
+    let valid = true;
+
+    const senha = this.formulario.querySelector('.senha');
+    const repetirSenha = this.formulario.querySelector('.repetir-senha');
+
+    if(senha.value !== repetirSenha.value) {
+      valid = false;
+      this.criaErro(senha, 'Campos senha e repetir senha precisar ser iguais.');
+      this.criaErro(repetirSenha, 'Campos senha e repetir senha precisar ser iguais.');
+    }
+
+    if(senha.value.length < 6 || senha.value.length > 12) {
+      valid = false;
+      this.criaErro(senha, 'Senha precisa estar entre 6 e 12 caracteres.');
+    }
+
+    return valid;
+  }
+
+  camposSaoValidos() {
+    let valid = true;
+
+    for(let errorText of this.formulario.querySelectorAll('.error-text')) {
+      errorText.remove();
+    }
+
+    for(let campo of this.formulario.querySelectorAll('.validar')) {
+      const label = campo.previousElementSibling.innerText;
+
+      if(!campo.value) {
+        this.criaErro(campo, `Campo "${label}" não pode estar em branco.`);
+        valid = false;
+      }
+
+      if(campo.classList.contains('cpf')) {
+        if(!this.validaCPF(campo)) valid = false;
+      }
+
+      if(campo.classList.contains('usuario')) {
+        if(!this.validaUsuario(campo)) valid = false;
+      }
 
     }
 
-    eventos(){
-        this,formulario.addEventListener('submit', e =>{
-            this.handleSubmit(e);
+    return valid;
+  }
 
-        });
+  validaUsuario(campo) {
+    const usuario = campo.value;
+    let valid = true;
+
+    if(usuario.length < 3 || usuario.length > 12) {
+      this.criaErro(campo, 'Usuário precisa ter entre 3 e 12 caracteres.');
+      valid = false;
     }
 
-    handleSubmit(e){
-        e.preventDefault();
-        const camposValidos = this.camposSaoValidos();
-        }
+    if(!usuario.match(/^[a-zA-Z0-9]+$/g)) {
+      this.criaErro(campo, 'Nome de usuário precisar conter apenas letras e/ou números.');
+      valid = false;
+    }
 
-        camposSaoValidos(){
-            let valid = true;
+    return valid;
+  }
 
-//Pega todos os elementos dentro de this.formulario e  O resultado é uma lista de nós (NodeList).
-//Vai percorrer cada elemento dessa lista, e em cada volta da repetição o elemento atual é guardado na variável campo.
+  validaCPF(campo) {
+    const cpf = new ValidaCPF(campo.value);
 
+    if(!cpf.valida()) {
+      this.criaErro(campo, 'CPF inválido.');
+      return false;
+    }
 
-            for(let campo of this.formulario.querySelectorAll('.validar')){
-                const label = campo.previousElementSibling.innerText; //Acessa o elemento irmão imediatamente anterior ao campo no HTML (geralmente um <label>).
-            
-//Esse código percorre todos os campos que precisam ser validados (.validar) e pega o texto do label imediatamente antes de cada campo, guardando na constante label.
+    return true;
+  }
 
-
-                if(! campo.value){
-                    this.criaErro(campo, 'Esse Campo não pode estar em branco.');
-                }
-            valid = false;
-            }
-        }
-
-        criaErro(campo , msg) {
-            const div = document.createElement('div');
-            div.innerHTML = msg;
-            div.classList.add('error - text ');
-            campo.insertAdjacenteElement('afterend');
-        }
+  criaErro(campo, msg) {
+    const div = document.createElement('div');
+    div.innerHTML = msg;
+    div.classList.add('error-text');
+    campo.insertAdjacentElement('afterend', div);
+  }
 }
 
 const valida = new ValidaFormulario();
-
-
-
-
